461be053887030425778ed3705e5ecd8cea93d9b
RefilweMk3 <refilwekgwadi13@gmail.com>
2024-08-29 21:19:26 +0200

    Push

---
+++ Flow charts in Python/main.py
@@ -0,0 +1,11 @@
+print("Hey, How you doing")
+
+print("I'm Refilwe Kgwadi")
+
+print(17,"yrs of age")
+
+print("Welcome to Python")
+
+print("Keywords in Python are....")
+import keyword
+print(keyword.kwlist)
